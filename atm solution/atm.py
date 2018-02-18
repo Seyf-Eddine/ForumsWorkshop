7e811a2e2795d7117e91b37360a19ecad78503b3
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-02-18 17:34:23 +0100

    adding withdraw function to atm

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -1,23 +1,22 @@
-money = 500
+def withdraw(money, request):
+	if request < money:
+		while request > 0 :
+			if request >= 100:
+				print "give 100"
+				request -= 100
+			elif request >= 50:
+				print "give 50"
+				request -= 50
+			elif request >= 10:
+				print "give 10"
+				request -= 10
+			elif request >= 5:
+				print "give 5"
+				request -= 5
+			else:
+				print "give "+str(request)
+				request -= request
+	else:
+		print "You don't have this money in your account!"
 
-request = 277
-
-if request < money:
-    while request > 0 :
-        if request >= 100:
-            print "give 100"
-            request -= 100
-        elif request >= 50:
-            print "give 50"
-            request -= 50
-        elif request >= 10:
-            print "give 10"
-            request -= 10
-        elif request >= 5:
-            print "give 5"
-            request -= 5
-        else:
-            print "give "+str(request)
-            request -= request
-else:
-    print "You don't have this money in your account!"
+withdraw(500,277)
