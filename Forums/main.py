bd8398a7baf13b544945cefd6b63677ea04f2fe5
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-02-20 15:33:43 +0100

    Adding Stores

--- Forums/main.py
+++ Forums/main.py
@@ -1,8 +1,18 @@
 import models
 
+memberStore = models.MemberStore()
+postStore = models.PostStore()
+
 member1 = models.Member("Seif Eddine", 26)
 member2 = models.Member("Mez", 22)
 
 post1 = models.Post("post1_title","here is post1 content this is a simple text")
 post2 = models.Post("post2_title","here is post2 content this is a simple text")
-post2 = models.Post("post2_title","here is post2 content this is a simple text")
+post3 = models.Post("post2_title","here is post2 content this is a simple text")
+
+memberStore.add(member1)
+memberStore.add(member2)
+
+postStore.add(post1)
+postStore.add(post2)
+postStore.add(post3)
--- Forums/models.py
+++ Forums/models.py
@@ -8,3 +8,25 @@ class Post():
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+
+class MemberStore():
+    members = []
+
+    def add(self, member):
+        MemberStore.members.append(member)
+
+    def get_all(self):
+        for member in MemberStore.members:
+            return member
+
+
+class PostStore():
+    posts = []
+
+    def add(self, post):
+        PostStore.posts.append(post)
+
+    def get_all(self):
+        for post in PostStore.posts:
+            return post
