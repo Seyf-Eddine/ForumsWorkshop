033fa23b2d3462059c900e62ac6ca87e3171bfe3
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-02-20 15:08:17 +0100

    Entities first commit

---
+++ Forums/main.py
@@ -0,0 +1,8 @@
+import models
+
+member1 = models.Member("Seif Eddine", 26)
+member2 = models.Member("Mez", 22)
+
+post1 = models.Post("post1_title","here is post1 content this is a simple text")
+post2 = models.Post("post2_title","here is post2 content this is a simple text")
+post2 = models.Post("post2_title","here is post2 content this is a simple text")
