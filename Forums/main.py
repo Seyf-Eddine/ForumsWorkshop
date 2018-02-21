a57d9daf9c31279d9eb79454369b2e5e3d2388cc
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-02-21 11:14:58 +0100

    Separated stores & edited get_all()

--- Forums/main.py
+++ Forums/main.py
@@ -1,14 +1,15 @@
 import models
+import stores
 
-memberStore = models.MemberStore()
-postStore = models.PostStore()
+memberStore = stores.MemberStore()
+postStore = stores.PostStore()
 
 member1 = models.Member("Seif Eddine", 26)
 member2 = models.Member("Mez", 22)
 
 post1 = models.Post("post1_title","here is post1 content this is a simple text")
 post2 = models.Post("post2_title","here is post2 content this is a simple text")
-post3 = models.Post("post2_title","here is post2 content this is a simple text")
+post3 = models.Post("post3_title","here is post2 content this is a simple text")
 
 memberStore.add(member1)
 memberStore.add(member2)
@@ -16,3 +17,9 @@ memberStore.add(member2)
 postStore.add(post1)
 postStore.add(post2)
 postStore.add(post3)
+
+for member in memberStore.get_all():
+    print member.name + " " + str(member.age)
+
+for post in postStore.get_all():
+    print post.title + " " + post.content
---
+++ Forums/stores.py
@@ -0,0 +1,17 @@
+class MemberStore():
+    members = []
+
+    def add(self, member):
+        self.members.append(member)
+
+    def get_all(self):
+        return self.members
+
+class PostStore():
+    posts = []
+
+    def add(self, post):
+        self.posts.append(post)
+
+    def get_all(self):
+        return self.posts
