6cba4852555caad3502353cdf8ad66fc4af91624
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-02-28 13:47:17 +0100

    Used Jinja
    Used Jinja and separated css file

--- Forums/app.py
+++ Forums/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import dummy_data
 
 app = Flask(__name__)
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/index")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", posts = dummy_data.post_store.get_all())
 
 
 app.run()
