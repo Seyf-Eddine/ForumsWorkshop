34662966a916995e4770a2a4308b3efede19b4d0
Seyf-Eddine <seyfeddine.mz@gmail.com>
2018-03-06 00:10:49 +0100

    Finished httpGet request

--- Project/forums/views.py
+++ Project/forums/views.py
@@ -1,4 +1,6 @@
-from flask import render_template
+from flask import render_template, request, url_for
+from werkzeug.utils import redirect
+
 from forums import models
 from forums import app, member_store, post_store
 
@@ -7,3 +9,13 @@ from forums import app, member_store, post_store
 @app.route("/index")
 def home():
     return render_template("index.html", posts=post_store.get_all())
+
+
+@app.route("/topic/add", methods = ["GET", "POST"])
+def topic_add():
+    if request.method == "POST":
+        new_post = models.Post(request.form["title"], request.form["content"])
+        post_store.add(new_post)
+        return redirect(url_for("home"))
+    else:
+        return render_template("topic_add.html")
